5af481a7b916b1e6825f085372d464acb745c505
Karim Karimov <kerimovscreations@gmail.com>
2024-02-10 10:42:52 +0400

    first commit

---
+++ app/src/main/java/az/edu/bhos/android/l5_gestures_sensors/MainActivity.kt
@@ -0,0 +1,11 @@
+package az.edu.bhos.android.l5_gestures_sensors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
